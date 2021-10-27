f88e2eb1f58327cbc9af087a10b61a385f9c6a7e
Roman Kolodiazhnyi <roman.kolodiazhnyi@altran.com>
2021-10-27 10:47:28 +0300

    Feedback fix

--- task1/config.js
+++ task1/config.js
@@ -1,13 +1,6 @@
 const args = process.argv.slice(2);
-const data = {};
-
-args.map((item, idx) => {
-    if (idx % 2 === 1) {
-        data[args[idx - 1].slice(2)] = item;
-    }
-});
 
 module.exports = {
     APP_PORT: process.env.APP_PORT || 4000,
-    ENV: data.env || 'default',
+    ENV: args[0] || 'default',
 }
